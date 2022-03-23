67437133eb796d8c378ab6da912907e8281ec665
Robin Munn <rmunn@pobox.com>
2022-03-23 08:52:01 +0700

    Add user page with custom content

---
+++ src/routes/userpage.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = async ({ props }) => {
+    console.log('load function of user page');
+    return { props };
+  }
+</script>
+
+<p>This is a user page that would have custom content for logged-in users.</p>
