93bbbfd74043291417e873761859d57b5af1b9b1
Robin Munn <rmunn@pobox.com>
2022-03-23 08:59:17 +0700

    Prerender the index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,6 @@
+<script context="module">
+  export const prerender = true;
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Go to a <a href="/userpage">user page</a> that would have custom content via getSession.</p>
